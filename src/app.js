dc3902a74f981632215af767cfecf2b077cf1ca7
hhhuilli <hhhuilli@tencent.com>
2022-07-20 10:03:50 +0800

    style: app.js格式

--- src/app.js
+++ src/app.js
@@ -16,10 +16,10 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use('/', router);
+  app.use('/', router);
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
